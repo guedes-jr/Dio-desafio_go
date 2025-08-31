5e5feb2ea57656e89fa1ee094d78f46d71a0512b
João Guedes <115195936+guedes-jr@users.noreply.github.com>
2025-08-31 20:12:03 +0000

    finalizado modulo de api

--- src/studies/api/poke.go
+++ src/studies/api/poke.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	//"os"
 )
+
+type Response struct {
+	name    string    `json: "name`
+	pokemon []pokemon `json: "pokemon_entries`
+}
+
+type pokemon struct {
+	numero  int            `json: "entry_number"`
+	especie pokemonSpecies `json: "pokemon_species"`
+}
+
+type pokemonSpecies struct {
+	name string `json: "name"`
+}
 
 func main() {
 	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto")
@@ -20,5 +35,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Print(string(responseData))
+	//fmt.Print(string(responseData))
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Print(responseObject.name)
+	fmt.Print((responseObject.pokemon))
+
+	for _, pokemon := range responseObject.pokemon {
+		fmt.Print(pokemon.especie.name)
+	}
 }
